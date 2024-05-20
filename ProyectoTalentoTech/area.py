ddad8e449434f3a12d260fbe644a1c32216366a4
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-05-20 18:58:39 -0500

    Update area.py
    s

--- ProyectoTalentoTech/area.py
+++ ProyectoTalentoTech/area.py
@@ -1,3 +1,5 @@
+from empleado import Empleado
+
 class Area:
 
     """---------------------
@@ -6,7 +8,7 @@ class Area:
     
     nombre=""
     Descripcion=""
-    ListaEmpealodsArea=[]
+    ListaEmpleadosArea=[]
 
     """--------------------------
     #Metodos
@@ -16,6 +18,21 @@ class Area:
         self.nombre = nombre
         self.Descripcion = descripcion
         self.ListaEmpleadosArea = []
-
+        
     def agregar_empleado(self, empleado):
         self.ListaEmpleadosArea.append(empleado)
+
+    def cuantosEmpleadosEnArea(self, pAreaY):
+    
+        count = 0
+        for empleado in self.empleados:
+            if empleado.area == pAreaY:
+                count += 1
+        return count
+    
+    def cuantosEmpleadosEnArea(self, pAreaX):
+        count = 0
+        for empleado in self.empleados:
+            if empleado.area == pAreaX:
+                count += 1
+        return count
