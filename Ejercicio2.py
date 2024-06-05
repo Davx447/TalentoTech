1d2f47f3d3de84378072fc7ed946ec0d22c7c7f9
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-06-05 13:04:51 -0500

    Delete Ejercicio2.py
    s

--- Ejercicio2.py
+++
@@ -1,9 +0,0 @@
-"I am the best software developter"
-print('"I\'m teh best software developter", she said')
-
-print("Su edad es: " + "25")
-print("Su edad es: " , 25, "y nacio en", 1999)
-
-# %s para cadenas
-# %d para numeros
-# %f para flotantes
