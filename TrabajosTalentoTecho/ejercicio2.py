81e14757eda97c7f4df2214b171afbb09d60c301
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-06-05 13:04:27 -0500

    Update and rename ds to ejercicio2.py
    s

---
+++ TrabajosTalentoTecho/ejercicio2.py
@@ -0,0 +1,10 @@
+
+"I am the best software developter"
+print('"I\'m teh best software developter", she said')
+
+print("Su edad es: " + "25")
+print("Su edad es: " , 25, "y nacio en", 1999)
+
+# %s para cadenas
+# %d para numeros
+# %f para flotantes
